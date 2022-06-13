bd6f011f7ce9283603982e097675b4b69b395a05
Mehrdad <mehrdadshirvan@gmail.com>
2022-06-14 00:30:18 +0430

    footer

---
+++ pages/include/header.js
@@ -0,0 +1,17 @@
+
+
+function Footer(props){
+    return (
+      <header>
+        <div className={`container`}>
+            <div className={`row`}>
+                <p className={`d-block text-center m-0`}>
+                    header
+                </p>
+            </div>
+        </div>
+      </header>
+    );
+}
+
+export default Footer;
