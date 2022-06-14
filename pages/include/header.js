18a0fa46045af6d3aee0360b860b81a544cecf1b
Mehrdad <mehrdadshirvan@gmail.com>
2022-06-15 00:55:06 +0430

    header

--- pages/include/header.js
+++ pages/include/header.js
@@ -1,16 +1,44 @@
+import Link from "next/link";
 
 
-function Footer(props){
+function Footer(props) {
     return (
-      <header>
-        <div className={`container`}>
-            <div className={`row`}>
-                <p className={`d-block text-center m-0`}>
-                    header
-                </p>
+        <header>
+            <div className={`container-fluid top-bar-bg bg-repeat-none bg-attachment-fixed bg-position-center`}>
+                <div className={`container`}>
+                    <div className={`row`}>
+                        <div className={``}></div>
+                    </div>
+                </div>
             </div>
-        </div>
-      </header>
+            <div className={`container`}>
+                <div className={`row`}>
+                    <ul>
+                        <li>
+                            <Link href={`/`}>
+                                <a className={``}>
+                                    خانه
+                                </a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href={`/about-us`}>
+                                <a className={``}>
+                                    درباره ما
+                                </a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href={`/contact-us`}>
+                                <a className={``}>
+                                    ارتباط ما
+                                </a>
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </header>
     );
 }
 
