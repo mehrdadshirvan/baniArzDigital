3d5f3b0835824c5d3b574271b3eb3c25088c1754
Mehrdad <mehrdadshirvan@gmail.com>
2022-06-16 19:21:06 +0430

    header & footer

--- pages/include/footer.js
+++ pages/include/footer.js
@@ -1,19 +1,33 @@
 import Link from 'next/link';
 
-function Footer(props){
+function Footer(props) {
     return (
-      <footer className={`bg-dark `}>
-        <div className={`container`}>
-            <div className={`row`}>
-                <p className={`d-block text-center m-0 text-white p-2`}>
-                    © Copyright
-                    <Link href="/" >
-                        <a className={`d-inline-block px-1 text-main text-decoration-none`}>baniarzdigital.com</a>
-                    </Link>
-                </p>
+        <footer className={``}
+                id={`footer`}>
+            <div className={`container py-3`}>
+                <div className={`row`}>
+                    <div className={`col-md-4`}>
+                        col-1
+                    </div>
+                    <div className={`col-md-4`}>
+                        col2
+                    </div>
+                    <div className={`col-md-4`}>
+                        col3
+                    </div>
+                </div>
             </div>
-        </div>
-      </footer>
+            <div className={`container`}>
+                <div className={`row`}>
+                    <p className={`d-block text-center m-0 text-white px-2 py-4`} dir={'ltr'}>
+                        © Copyright
+                        <Link href="/">
+                            <a className={`d-inline-block px-1 text-white text-decoration-none`}>baniarzdigital.com</a>
+                        </Link>
+                    </p>
+                </div>
+            </div>
+        </footer>
     );
 }
 
--- pages/include/header.js
+++ pages/include/header.js
@@ -1,41 +1,53 @@
 import Link from "next/link";
-
+// import '../styles/header.globals.module.css'
 
 function Footer(props) {
     return (
-        <header>
-            <div className={`container-fluid top-bar-bg bg-repeat-none bg-attachment-fixed bg-position-center`}>
+        <header id={`header`}>
+            <div id={'top-bar'} className={`container-fluid top-bar-bg bg-repeat-none bg-attachment-fixed bg-position-center`}>
                 <div className={`container`}>
                     <div className={`row`}>
-                        <div className={``}></div>
+                        <div className={`col-12`}>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div className={`container`}>
+            <div className={`container`} id={`main-header`}>
                 <div className={`row`}>
-                    <ul>
-                        <li>
-                            <Link href={`/`}>
-                                <a className={``}>
-                                    خانه
-                                </a>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href={`/about-us`}>
-                                <a className={``}>
-                                    درباره ما
-                                </a>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href={`/contact-us`}>
-                                <a className={``}>
-                                    ارتباط ما
-                                </a>
-                            </Link>
-                        </li>
-                    </ul>
+                    <div className={`col-md-9 menu`}>
+                        <ul>
+                            <li>
+                                <Link href={`/`}>
+                                    <a className={``}>
+                                        خانه
+                                    </a>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href={`/about-us`}>
+                                    <a className={``}>
+                                        درباره ما
+                                    </a>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href={`/contact-us`}>
+                                    <a className={``}>
+                                        ارتباط ما
+                                    </a>
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div className={`col-md-3 logo d-grid align-content-center`}>
+                        <Link href={'/'}>
+                            <a className={'d-block text-left'}>
+                                <img src="./images/logo-light.png"
+                                     alt="بانی ارز دیجیتال"/>
+                            </a>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </header>
