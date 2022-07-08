db61d304739a85e3c9b82e5debdfbadd283290b9
Mehrdad <mehrdadshirvan@gmail.com>
2022-07-08 19:30:37 +0430

    search

--- pages/index.js
+++ pages/index.js
@@ -137,7 +137,10 @@ function Home(props) {
 }
 
 export async function getServerSideProps(context) {
-    const res = await fetch('http://127.0.0.1:8000/api/v1/home?tt=7Cm8Yiyz1OG2QEsRoWO3MU1SN4Be8wQdSEEElsJKft3b7vtP0jlAYjzBo0Kcs8W5Ux84GnnpwXGZcC2RgwYbOyh0CmXedmuyGCBD');
+    const tt = '7Cm8Yiyz1OG2QEsRoWO3MU1SN4Be8wQdSEEElsJKft3b7vtP0jlAYjzBo0Kcs8W5Ux84GnnpwXGZcC2RgwYbOyh0CmXedmuyGCBD';
+    const baseURL = 'https://banibank.com';
+    const url = baseURL + '/api/v1/home?tt=' + tt;
+    const res = await fetch(url);
     const home = await res.json();
 
     return {
